3f968bec8ab99261c955c12f79ba4e33755aef6e
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-12 09:32:46 -0400

    Gigglator_rev_16

--- js/giggle_script.js
+++ js/giggle_script.js
@@ -4,7 +4,7 @@ var myGigtotal = myGiggles * 5; // calculates the total of giggles times 5 to ge
 var myComedy = prompt("Each giggle on average lasts 5 seconds. Please enter the rough amount of comedy you watch per week in hours."); // user enters the amount of comedy they watch per week.
 var myLaff = myGigtotal * myComedy; // calculates the total of giggles times the amount of comedy you watch per week.
 // console.log(myLaff); // prints out to the console.
-var resultGigPW = "You giggle on average " + myLaff + " times per week! Can you believe it?"; // 
+var resultGigPW = "You giggle on average " + myLaff + " times per week! Can you believe it?"; // Javascript displays the totals. 
 alert(resultGPW); // outputs the result in an alert box in the browser.
 var myYVisitors = prompt("Go back to that same YouTube video you choose. Enter the amount of people who watched the video."); //user enters the amount of visitors that watched the video on YouTube.
 var myVGT = myYVisitors * myLaff; // calculates the total of giggles per week of you and the YouTube visitors.
