a0fe06fd7363668f08aaa02dc3c570391b62e1ab
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-12 09:09:52 -0400

    Gigglator_rev_8

--- js/giggle_script.js
+++ js/giggle_script.js
@@ -4,9 +4,9 @@ var myGigtotal = myGiggles * 5; // calculates the total of giggles times 5 to ge
 var myComedy = prompt("Each giggle on average lasts 5 seconds. Please enter the rough amount of comedy you watch per week in hours."); //user enters the amount of comedy they watch per week.
 var myLaff = myGigtotal * myComedy; // calculates the total of giggles times the amount of comedy you watch per week.
 //console.log(myLaff); //prints out to the console.
-var resultGigPW = "You giggle on average " + myGPD + "Gallons of gas per day to drive to work.";
-alert(resultGPD);
-var myDays = prompt("How many days do you work per week? Please enter the total days.") * 50; //user enters the weekly amount of days they work.
+var resultGigPW = "You giggle on average " + myLaff + " per week! Can you believe it?";
+alert(resultGPW);
+var myYVisitors = prompt("Go back to that same YouTube video you choose. Enter the amount of people who watched the video."); //user enters the weekly amount of days they work.
 var myGTY = myDays * myGPD; // calculates the total of yealy days multiplied by the vehicle's gallons per day.
 //console.log(myGTY); //prints out to the console.
 var resultGTY = "You use roughly " + myGTY + "Gallons of gas per year to drive to work."; //Javascript displays the totals
