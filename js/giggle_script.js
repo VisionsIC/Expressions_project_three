081c9c657bde80bfef9de71a0b46271cc9a60a07
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-12 09:34:29 -0400

    Gigglator_rev_17

--- js/giggle_script.js
+++ js/giggle_script.js
@@ -10,7 +10,7 @@ var myYVisitors = prompt("Go back to that same YouTube video you choose. Enter t
 var myVGT = myYVisitors * myLaff; // calculates the total of giggles per week of you and the YouTube visitors.
 // console.log(myVGT); // prints out to the console.
 var resultVGT = "You and all your fellow YouTube visitors giggle " + myVGT + " times per week! That's a lot of belly flutters! :) "; // Javascript displays the totals.
-alert(resultGTY);
+alert(resultVGT);
 var myGPrice = prompt("How much is the cost of a gallon of gas at your local gas station? Enter the Price Per Gallon. $"); //user enters the price of gas per gallon.
 myFloat = parseFloat(myGPrice);
 var myGETY = myFloat * myGTY; // calculates the total of yearly cost of gas.
