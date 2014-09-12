1d310a210dbf7be49bf3049436bdf13660b55e08
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-12 09:29:35 -0400

    Gigglator_rev_14

--- js/giggle_script.js
+++ js/giggle_script.js
@@ -4,12 +4,12 @@ var myGigtotal = myGiggles * 5; // calculates the total of giggles times 5 to ge
 var myComedy = prompt("Each giggle on average lasts 5 seconds. Please enter the rough amount of comedy you watch per week in hours."); // user enters the amount of comedy they watch per week.
 var myLaff = myGigtotal * myComedy; // calculates the total of giggles times the amount of comedy you watch per week.
 // console.log(myLaff); // prints out to the console.
-var resultGigPW = "You giggle on average " + myLaff + " per week! Can you believe it?"; // 
+var resultGigPW = "You giggle on average " + myLaff + " times per week! Can you believe it?"; // 
 alert(resultGPW); // outputs the result in an alert box in the browser.
 var myYVisitors = prompt("Go back to that same YouTube video you choose. Enter the amount of people who watched the video."); //user enters the amount of visitors that watched the video on YouTube.
-var myVGT = myYVisitors * myLaff; // calculates the total of giggles per week of you and the youtube visitors.
-// console.log(myGTY); // prints out to the console.
-var resultGTY = "You use roughly " + myGTY + "Gallons of gas per year to drive to work."; //Javascript displays the totals
+var myVGT = myYVisitors * myLaff; // calculates the total of giggles per week of you and the YouTube visitors.
+// console.log(myVGT); // prints out to the console.
+var resultVGT = "You and all your fellow YouTube visitors giggle " + myVGT + " times per week! That's a lot of belly flutters! :) "; //Javascript displays the totals
 alert(resultGTY);
 var myGPrice = prompt("How much is the cost of a gallon of gas at your local gas station? Enter the Price Per Gallon. $"); //user enters the price of gas per gallon.
 myFloat = parseFloat(myGPrice);
